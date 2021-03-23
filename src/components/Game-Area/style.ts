e06dc297c551c1fac43b071e800c957cfdf64407
FocusCript <luckerfocus@mail.ru>
2021-03-23 13:50:04 +0500

    game area styled

---
+++ src/components/Game-Area/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position: relative;
+  margin: 50px auto;
+  width: 600px;
+  height: 600px;
+  background: rgba(255, 0, 212, 0.137);
+  border: 2px solid #000;
+`
